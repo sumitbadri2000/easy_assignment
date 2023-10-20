40901766d66fb671c4097ab7ddd6b0296164e877
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-10-20 19:15:21 +0530

    eazy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import "./App.css";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+
+function TextItem(props) {
+  return (
+    <div>
+      <p>Name: {props.names[props.index]}</p>
+      <p>Age: {props.ages[props.index]}</p>
+      <Button
+        onClick={() =>
+          props.updateDetails(props.names[props.index], props.age, props.index)
+        }>
+        Reset Details
+      </Button>
+    </div>
+  );
+}
 
 function App() {
+  const [names, setNames] = useState(["Hello"]);
+  const [ages, setAges] = useState([23]);
+
+  function addItem() {
+    const name = Math.random().toString(36).substring(2, 7);
+    setNames([...names, name]);
+    const age = Math.floor(Math.random() * 100);
+    setAges([...ages, age]);
+  }
+
+  function updateDetails(name, age, index) {
+    //Remove current name and replace with blank
+    let currentName = names[index];
+    let currentAge = ages[index];
+    let namearray = names.filter((item) => item != currentName);
+    let agearray = ages.filter((item) => item !== currentAge);
+    namearray.push("");
+    agearray.push("");
+    setNames(namearray);
+    setAges(agearray);
+  }
+
+  //Whenever name is updated, print it
+  console.log(names);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to the EazyDraft test application.</p>
+        <div onClick={addItem}>Click to add a field</div>
+        {names.map((name, index) => (
+          <TextItem
+            names={names}
+            ages={ages}
+            index={index}
+            updateDetails={updateDetails}
+          />
+        ))}
       </header>
     </div>
   );
